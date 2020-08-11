497b48e5bd119c9389ed54e5ac200142f7dd148b
duyuqian <809009017@qq.com>
2020-08-11 12:07:48 +0800

    [duyuqian] feat: add the frameActivity

---
+++ app/src/main/java/com/thoughtworks/layout/FrameActivity.java
@@ -0,0 +1,14 @@
+package com.thoughtworks.layout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FrameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_frame);
+    }
+}
