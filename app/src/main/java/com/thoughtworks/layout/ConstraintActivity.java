4b23ef6e49fdd0fd48448076cdea13b2c7ce9d79
duyuqian <809009017@qq.com>
2020-08-11 18:39:04 +0800

    [duyuqian] feat: skip to MainActivity when click the login button

--- app/src/main/java/com/thoughtworks/layout/ConstraintActivity.java
+++ app/src/main/java/com/thoughtworks/layout/ConstraintActivity.java
@@ -3,6 +3,8 @@ package com.thoughtworks.layout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ConstraintActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class ConstraintActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_layout);
+
+        Button login = findViewById(R.id.login);
+        login.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        finish();
+                    }
+                }
+        );
     }
 }
